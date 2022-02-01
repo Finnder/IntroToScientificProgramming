c8c5d93089db0de4e1d147710c93e2dd307999d9
Finnder <mcguirefinneganftw@gmail.com>
2022-02-01 14:06:21 -0500

    Made a 'labs' folder

--- lab00/haiku.py
+++
@@ -1,9 +0,0 @@
-
-lines = ['Just nine lines of code', 'Did I forget my syntax', 'Compiler error']
-
-for line in lines:
-    print(line)
-
-
-
-
--- lab00/introductions.py
+++
@@ -1,5 +0,0 @@
-message = """
-Hi, my name is Finn - I am taking this course because I personally like to program and have been for awhile now. I am a CS major mainly taking this course for fun and for a gen ed requirement. My hobbies are game development, playing video games , and working out.
-"""
-
-print(message)
