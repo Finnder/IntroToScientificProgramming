abbb19bf88f89858bfc131dfee0a41915add2329
Finnder <mcguirefinneganftw@gmail.com>
2022-02-21 12:43:28 -0500

    Finilized Decrypt.py

--- labs/lab04/decrypt.py
+++ labs/lab04/decrypt.py
@@ -1,6 +1,13 @@
+# Decrypt
+# By: Finnegan McGuire
+# Status: Complete
+
+
+# Open files
 inpf = open("lab04-materials/book.txt", "r", encoding="utf8", errors="ignore")
 outf = open("lab04-materials/utput.txt", "w", encoding="utf8", errors="ignore")
 
+# Define a list of the different letters needed (the alphabet, Uppercase and Lowercase)
 lower_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 upper_alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
 
@@ -13,38 +20,39 @@ outf.truncate(0) # Erase previous file contents
 
 
 # Returns -> String / Char
-def IncrementCharacter(char):
-
-    newChar = char
-    amountToDecrease = 3
-    check = False
-
-    lowerCase = False
-    upperCase = False
-    # TODO: For some reason these if statements are always resulting to false, even when it should be true
+def FormatCharacter(char):
     
-    # Is character lowercase?
+    # Initialize variables
+    newChar = char
+    amountToDecrease = 3 # Amount to decrease letter (ex: d -> a)
+    lowerCase = False 
+    upperCase = False 
+
+    # TODO: For some reason these if statements are always resulting to false, even when it should be true
+   
+    # Check is char is lowercase
     for chars in lower_alphabet:
         if char == chars:
             lowerCase = True
-
-    # Is character uppercase?
+    
+    # Check if char is uppercase
     for chars in upper_alphabet:
         if char == chars:
             upperCase = True
-
+    
+    # If char is lowercase
     if lowerCase == True:
         newCharIndex = lower_alphabet.index(char) - amountToDecrease
         newChar = lower_alphabet[newCharIndex]    
-        check = True
 
+    # If char is uppercase
     if upperCase == True:
         newCharIndex = upper_alphabet.index(char) - amountToDecrease
         newChar = upper_alphabet[newCharIndex]
-        check = True
     
     return newChar
 
+# Coverts character
 def ChangeCharacter(char):
     x = ord(char)
     y = chr(x)
@@ -53,14 +61,19 @@ def ChangeCharacter(char):
 # For every character in inpf file
 for c in inpf.read():
 
-    # Coverts unknown characters into normal characters
+    # Coverts characters
     z = ChangeCharacter(c) 
 
     # Increments letter by 3 and adds to output file
-    output = IncrementCharacter(z)
-
+    output = FormatCharacter(z)
+    
+    # Append decrypted character to output file
     outf.write(output)
 
-# Close files
+
+# Close files (good practice)
 outf.close()
 inpf.close()
+
+
+
