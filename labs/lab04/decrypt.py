5483e9675bebfce3fa2998942ea8653b46d7c7f5
Finnder <mcguirefinneganftw@gmail.com>
2022-02-21 09:56:47 -0500

    Started decrypt script

--- labs/lab04/decrypt.py
+++ labs/lab04/decrypt.py
@@ -1,10 +1,20 @@
 inpf = open("lab04-materials/book.txt", "r", encoding="utf8", errors="ignore")
 outf = open("lab04-materials/utput.txt", "w", encoding="utf8", errors="ignore")
 
+
+lower_alphabet = string.ascii_lowercase # Gives all lowercase letters of the alphabet
+upper_alphabet = string.ascii_uppercase # Gives all uppercase letters of the alphabet
+
+
 # TODO: Decrypt the book
 for c in inpf:
-    output = ""
+    output = []
+    if c == any(lower_alphabet) or c == any(upper_alphabet):
+        x = ord(c)
+        y = chr(x)
+        output.append(y)
 
+print(output)
 
 outf.close()
 inpf.close()
