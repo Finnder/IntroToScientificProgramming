2f29f11d177c25e8017ca0689640917df4d091a1
Finnder <mcguirefinneganftw@gmail.com>
2022-04-27 13:52:08 -0400

    added txt files

---
+++ Final/above-average.py
@@ -0,0 +1,17 @@
+"""
+Sample Output:
+
+Enter input file name: small-average.txt
+The average is 71.3
+The percentage of people above average is 60.0
+The student is above the average
+"""
+
+fileName = input('Enter input file name: ')
+
+with open(fileName, 'r') as file:
+    print(file)
+
+def calcAvg(data):
+    pass
+
---
+++ Final/binary-to-hex.py
@@ -0,0 +1,15 @@
+"""
+Sample Input and Output:
+Enter a binary string: 10101001101011
+The hex value of the string is 2a6b
+"""
+
+binString = input('Enter a binary string: ')
+
+def CalcBinary():
+    pass
+`
+
+
+for i in range(0, 3):
+
